ef9bd9e532180489618314c4472aa2bc9c6d3fe7
Eletrax <spencerlenahan@gmail.com>
2022-10-06 21:40:50 -0500

    10/6/22

---
+++ Java/JavaNotCoffee/Assignments/GroceryList/GroceryItem.java
@@ -0,0 +1,33 @@
+public class GroceryItem {
+
+     private String name;
+     private double cost;
+
+     //need the no arg constructor technically
+
+     public GroceryItem(String name,double cost){
+          this.name=name;
+          this.cost=cost;
+     }
+
+     //getters and setters
+     public void setName(String newName){
+          this.name=newName;
+     }
+     public String getName(){
+          return this.name;
+     }
+     public void setCost(double newCost){
+          this.cost=newCost;
+     }
+     public double getCost(){
+          return this.cost;
+     }
+
+     //toString
+     @Override
+     public String toString(){
+          return this.name +" - "+String.valueOf(this.cost);
+     }
+
+}
