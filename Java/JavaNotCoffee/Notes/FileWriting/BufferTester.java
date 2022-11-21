39887c431281eea567b61deee63ec0172dae223f
Lenahan2021 <spencer.lenahan@evsck12.com>
2022-11-21 11:42:07 -0600

    Fixing git

---
+++ Java/JavaNotCoffee/Notes/FileWriting/BufferTester.java
@@ -0,0 +1,12 @@
+
+
+public class BufferTester {
+    //Read in the file and print to the console
+    public static void main(String[] args) {
+        //write a file
+        BufferWriter.writeString("Hello World!");
+        BufferWriter.saveAndClose();
+        //read in the file and print to the console
+        System.out.println(BufferWriter.readString());   
+    }
+}
